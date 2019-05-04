8ebd941fca74afd588b3c1458e632e6f377c9b8a
ismail bayram <ismailbayram.ce@gmail.com>
2019-05-04 11:54:12 +0300

    AutoPark Class created

---
+++ src/AutoPark.java
@@ -0,0 +1,26 @@
+public class AutoPark {
+//	private SubscribedVehicle[] subscribedVehicles;
+	private ParkRecord[] parkRecords;
+	private double hourlyFee, incomeDaily;
+	
+	public AutoPark(double hourlyFee, double incomeDaily) {
+		this.hourlyFee = hourlyFee;
+		this.incomeDaily = incomeDaily;
+	}
+
+	public double getHourlyFee() {
+		return hourlyFee;
+	}
+
+	public void setHourlyFee(double hourlyFee) {
+		this.hourlyFee = hourlyFee;
+	}
+
+	public double getIncomeDaily() {
+		return incomeDaily;
+	}
+
+	public void setIncomeDaily(double incomeDaily) {
+		this.incomeDaily = incomeDaily;
+	}
+}
