594e291520d09894f078d17f0637a64661198786
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-05-05 18:09:46 +0300

    Updated Main class

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,12 @@ public class Main {
 		System.out.println("Hello World!");
 		Date d1 = new Date(13,2, 2019);
 		Date d2 = new Date(13,6, 2019);
+		AutoPark autoPark = new AutoPark(2.0, 100);
 		Subscription sub = new Subscription(d1, d2, "34FH3773");
+		System.out.println(autoPark.searchVehicle("34FH3773"));
+		SubscribedVehicle vehicle = sub.getVehicle();
+		autoPark.addVehicle(vehicle);
 		System.out.println(sub.getVehicle().getSubscription().isValid());
+		System.out.println(autoPark.searchVehicle("34FH3773"));
 	}
 }
