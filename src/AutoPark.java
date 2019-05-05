7e1a91efff4897f0ac6001512c549c5bb8df1b09
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-05-05 18:12:50 +0300

    Added functions in AutoPark class

--- src/AutoPark.java
+++ src/AutoPark.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
+
 public class AutoPark {
-	private SubscribedVehicle[] subscribedVehicles;
-	private ParkRecord[] parkRecords;
-	private double hourlyFee, incomeDaily;
+	private ArrayList<SubscribedVehicle> subscribedVehicles;
+	private ArrayList<ParkRecord> parkRecords;
+	private ArrayList<Vehicle> vehicles;
 	
-	public AutoPark(double hourlyFee, double incomeDaily) {
+	private double hourlyFee, incomeDaily;
+	private int capacity;
+	
+	public AutoPark(double hourlyFee, int capacity) {
 		this.hourlyFee = hourlyFee;
-		this.incomeDaily = incomeDaily;
+		this.capacity = capacity;
+		this.incomeDaily = 0.0;
+		this.subscribedVehicles = new ArrayList<>();
+		this.vehicles = new ArrayList<>();
+		this.parkRecords = new ArrayList<>();
 	}
 
 	public double getHourlyFee() {
@@ -23,4 +32,74 @@ public class AutoPark {
 	public void setIncomeDaily(double incomeDaily) {
 		this.incomeDaily = incomeDaily;
 	}
+	
+	public SubscribedVehicle searchVehicle(String plate) {
+		for(SubscribedVehicle subscribedVehicle: subscribedVehicles) {
+			if(plate.equals(subscribedVehicle.getPlate())) {
+				return subscribedVehicle;
+			}
+		}
+		return null;
+	}
+	public boolean addVehicle(SubscribedVehicle subscribedVehicle) {
+		if(subscribedVehicles.size() == capacity) {
+			this.enLargeVehicleArray();
+			System.out.println("Kapasite arttirildi");
+		}
+		if(this.searchVehicle(subscribedVehicle.getPlate()) == null) {
+			subscribedVehicles.add(subscribedVehicle);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private void enLargeVehicleArray() {
+		this.capacity += this.capacity;
+	}
+	public boolean isParked(String plate) {
+		for(Vehicle vehicle: vehicles) {
+			if(plate.equals(vehicle.getPlate())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public boolean vehicleEnters(String plate, Time enter, boolean isOfficial) {
+		if(isOfficial)
+			return true;
+		
+		if(this.isParked(plate))
+			return false;
+		
+		if(this.searchVehicle(plate) != null)
+			if(this.searchVehicle(plate).getSubscription().isValid()) {
+				vehicles.add(this.searchVehicle(plate));
+				return true;
+			}
+		Vehicle vehicle = new RegularVehicle(plate);
+		vehicles.add(vehicle);
+		ParkRecord parkRecord = new ParkRecord(enter, vehicle);
+		parkRecords.add(parkRecord);
+		return true;
+	}
+	public ParkRecord searchParkRecord(Vehicle vehicle) {
+		for(ParkRecord parkRecord: parkRecords) {
+			if(vehicle.equals(parkRecord.getVehicle()))
+				return parkRecord;
+		}
+		return null;
+	}
+	public boolean vehicleExits(String plate, Time exit) {
+		for(Vehicle vehicle: vehicles) {
+			if(plate.equals(vehicle.getPlate())) {
+				if(vehicle.getSubscription() == null) {
+					ParkRecord parkRecord = this.searchParkRecord(vehicle);
+					this.incomeDaily += this.hourlyFee * (parkRecord.getEnterTime().getDifference(exit));
+				}
+			}
+		}
+		return true;
+	}
+	
 }
